7b499cf72a3c48685c57900a4d3d4cdf320c2a5d
Jade Stewart <jadekstewart3@gmail.com>
2023-07-14 18:26:05 -0600

    remove comments

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,6 +1,4 @@
 class Unicorn {
-  // Think of a constructor as being similar to initialize in Ruby
-  // Instead of setting attributes like @name = name in JavaScript we say this.name = name
   constructor(name, color = "white") {
     this.name = name;
     this.color = color;
@@ -16,8 +14,4 @@ class Unicorn {
     }
   }
 }
-
-
-// Notice here we are exporting our class.
-// In order for our test or any other file to be able to know about it we must export it.
 module.exports = Unicorn;
