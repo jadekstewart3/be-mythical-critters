fa0ae5f99b79dd266b14bec4557f29e6a0ea8c70
Jade Stewart <jadekstewart3@gmail.com>
2023-07-17 13:15:28 -0600

    centaur solution

--- exercises/centaur.js
+++ exercises/centaur.js
@@ -1,31 +1,79 @@
 class Centaur {
-  constructor(name, breed, cranky = false, standing = true, counter = 0) {
+  constructor(name, breed) {
     this.name = name;
     this.breed = breed;
-    this.cranky = cranky;
-    this.standing = standing;
-    this.counter = counter;
+    this.cranky = false;
+    this.standing = true;
+    this.layingDown = !this.standing;
+    this.activity = 0;
   }
 
   shoot() {
-    if(this.cranky === true) {
-      return 'NO!';
-    } 
-    else {
-      this.counter++;
-      return 'Twang!!!';
+    this.activity++;
+    if (this.activity < 3 && this.standing) {
+      return "Twang!!!";
+    } else {
+      return this.no();
     }
   }
 
   run() {
-    this.counter++;
-    return 'Clop clop clop clop!!!';
-  }
-
-  cranky() {
-    if (this.shoot + this.run >= 3) {
-      return true;
+    this.activity++;
+    if (this.activity < 3 && this.standing) {
+      return "Clop clop clop clop!!!";
+    } else {
+      return this.no();
     }
   }
+
+  layDown() {
+    this.layingDown = true;
+    this.standing = false;
+  }
+
+  standUp() {
+    this.layingDown = false;
+    this.standing = true;
+  }
+
+  sleep() {
+    if (this.standing) {
+      return "NO!";
+    } 
+    else {
+      this.activity = 0;
+      this.cranky = false;
+      return "ZZZZ";
+    }
+  }
+
+  drinkPotion() {
+    switch (this.statusChecker()) {
+      case "Standing and not cranky":
+        this.cranky = true;
+        break;
+      case "Standing and cranky":
+        this.activity = 0;
+        this.cranky = false;
+        break;
+      case "Not standing":
+        return 'Not while I\'m laying down!';
+    }
+  }
+
+  statusChecker() {
+    if (this.layingDown) {
+      return "Not standing";
+    } else if (this.standing && this.cranky) {
+      return "Standing and cranky";
+    } else {
+      return "Standing and not cranky";
+    }
+  }
+
+  no() {
+    this.cranky = true;
+    return "NO!";
+  }
 }
- module.exports = Centaur;
+module.exports = Centaur;
